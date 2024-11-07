e0e12856458298469bb5a2428e0dcc32923eb7ab
Jasdeep-S <96511236+Jasdeep-S@users.noreply.github.com>
2024-11-07 17:53:17 -0500

    Add files via upload

---
+++ Visitor.Designer.vb
@@ -0,0 +1,258 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Visitor
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        ComboBox1 = New ComboBox()
+        ComboBox2 = New ComboBox()
+        ComboBox3 = New ComboBox()
+        ComboBox4 = New ComboBox()
+        ComboBox5 = New ComboBox()
+        ComboBox6 = New ComboBox()
+        ComboBox7 = New ComboBox()
+        ComboBox8 = New ComboBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        ComboBox9 = New ComboBox()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        ComboBox10 = New ComboBox()
+        ComboBox11 = New ComboBox()
+        ComboBox12 = New ComboBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = SystemColors.Highlight
+        Label1.Font = New Font("Segoe UI", 25F)
+        Label1.Location = New Point(26, 20)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(284, 46)
+        Label1.TabIndex = 0
+        Label1.Text = "Design Your Tour!"
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"Canada,", "United States of America", "Mexico", "Argentina", "Bahrain", "Denmark", "Egypt", "Finland", "Greece", "Honduras", "Iceland", "Japan", "Kenya"})
+        ComboBox1.Location = New Point(22, 98)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(194, 23)
+        ComboBox1.TabIndex = 1
+        ' 
+        ' ComboBox2
+        ' 
+        ComboBox2.FormattingEnabled = True
+        ComboBox2.Items.AddRange(New Object() {"2020", "2099", "3000", "1953", "1823", "1875", "1828", "2949", "2019", "1747", "1929", "2222", "4828"})
+        ComboBox2.Location = New Point(239, 98)
+        ComboBox2.Name = "ComboBox2"
+        ComboBox2.Size = New Size(78, 23)
+        ComboBox2.TabIndex = 2
+        ' 
+        ' ComboBox3
+        ' 
+        ComboBox3.FormattingEnabled = True
+        ComboBox3.Items.AddRange(New Object() {"2020", "2099", "3000", "1953", "1823", "1875", "1828", "2949", "2019", "1747", "1929", "2222", "4828"})
+        ComboBox3.Location = New Point(239, 156)
+        ComboBox3.Name = "ComboBox3"
+        ComboBox3.Size = New Size(78, 23)
+        ComboBox3.TabIndex = 4
+        ' 
+        ' ComboBox4
+        ' 
+        ComboBox4.FormattingEnabled = True
+        ComboBox4.Items.AddRange(New Object() {"Canada,", "United States of America", "Mexico", "Argentina", "Bahrain", "Denmark", "Egypt", "Finland", "Greece", "Honduras", "Iceland", "Japan", "Kenya"})
+        ComboBox4.Location = New Point(22, 156)
+        ComboBox4.Name = "ComboBox4"
+        ComboBox4.Size = New Size(194, 23)
+        ComboBox4.TabIndex = 3
+        ' 
+        ' ComboBox5
+        ' 
+        ComboBox5.FormattingEnabled = True
+        ComboBox5.Items.AddRange(New Object() {"2020", "2099", "3000", "1953", "1823", "1875", "1828", "2949", "2019", "1747", "1929", "2222", "4828"})
+        ComboBox5.Location = New Point(239, 213)
+        ComboBox5.Name = "ComboBox5"
+        ComboBox5.Size = New Size(78, 23)
+        ComboBox5.TabIndex = 6
+        ' 
+        ' ComboBox6
+        ' 
+        ComboBox6.FormattingEnabled = True
+        ComboBox6.Items.AddRange(New Object() {"Canada,", "United States of America", "Mexico", "Argentina", "Bahrain", "Denmark", "Egypt", "Finland", "Greece", "Honduras", "Iceland", "Japan", "Kenya"})
+        ComboBox6.Location = New Point(22, 213)
+        ComboBox6.Name = "ComboBox6"
+        ComboBox6.Size = New Size(194, 23)
+        ComboBox6.TabIndex = 5
+        ' 
+        ' ComboBox7
+        ' 
+        ComboBox7.FormattingEnabled = True
+        ComboBox7.Items.AddRange(New Object() {"2020", "2099", "3000", "1953", "1823", "1875", "1828", "2949", "2019", "1747", "1929", "2222", "4828"})
+        ComboBox7.Location = New Point(239, 275)
+        ComboBox7.Name = "ComboBox7"
+        ComboBox7.Size = New Size(78, 23)
+        ComboBox7.TabIndex = 8
+        ' 
+        ' ComboBox8
+        ' 
+        ComboBox8.FormattingEnabled = True
+        ComboBox8.Items.AddRange(New Object() {"Canada,", "United States of America", "Mexico", "Argentina", "Bahrain", "Denmark", "Egypt", "Finland", "Greece", "Honduras", "Iceland", "Japan", "Kenya"})
+        ComboBox8.Location = New Point(22, 275)
+        ComboBox8.Name = "ComboBox8"
+        ComboBox8.Size = New Size(194, 23)
+        ComboBox8.TabIndex = 7
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI", 12F)
+        Button1.Location = New Point(22, 323)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 31)
+        Button1.TabIndex = 9
+        Button1.Text = "Confirm"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(657, 12)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(82, 39)
+        Button2.TabIndex = 10
+        Button2.Text = "Take Photo"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' ComboBox9
+        ' 
+        ComboBox9.FormattingEnabled = True
+        ComboBox9.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "", ""})
+        ComboBox9.Location = New Point(334, 98)
+        ComboBox9.Name = "ComboBox9"
+        ComboBox9.Size = New Size(53, 23)
+        ComboBox9.TabIndex = 11
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(22, 80)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(53, 15)
+        Label2.TabIndex = 15
+        Label2.Text = "Location"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(239, 80)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(29, 15)
+        Label3.TabIndex = 16
+        Label3.Text = "Year"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(334, 80)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(53, 15)
+        Label4.TabIndex = 17
+        Label4.Text = "Duration"
+        ' 
+        ' ComboBox10
+        ' 
+        ComboBox10.FormattingEnabled = True
+        ComboBox10.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "", ""})
+        ComboBox10.Location = New Point(334, 213)
+        ComboBox10.Name = "ComboBox10"
+        ComboBox10.Size = New Size(53, 23)
+        ComboBox10.TabIndex = 18
+        ' 
+        ' ComboBox11
+        ' 
+        ComboBox11.FormattingEnabled = True
+        ComboBox11.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "", ""})
+        ComboBox11.Location = New Point(334, 156)
+        ComboBox11.Name = "ComboBox11"
+        ComboBox11.Size = New Size(53, 23)
+        ComboBox11.TabIndex = 19
+        ' 
+        ' ComboBox12
+        ' 
+        ComboBox12.FormattingEnabled = True
+        ComboBox12.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "", ""})
+        ComboBox12.Location = New Point(334, 275)
+        ComboBox12.Name = "ComboBox12"
+        ComboBox12.Size = New Size(53, 23)
+        ComboBox12.TabIndex = 20
+        ' 
+        ' Visitor
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(751, 450)
+        Controls.Add(ComboBox12)
+        Controls.Add(ComboBox11)
+        Controls.Add(ComboBox10)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(ComboBox9)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(ComboBox7)
+        Controls.Add(ComboBox8)
+        Controls.Add(ComboBox5)
+        Controls.Add(ComboBox6)
+        Controls.Add(ComboBox3)
+        Controls.Add(ComboBox4)
+        Controls.Add(ComboBox2)
+        Controls.Add(ComboBox1)
+        Controls.Add(Label1)
+        Name = "Visitor"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Visitor"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents ComboBox4 As ComboBox
+    Friend WithEvents ComboBox5 As ComboBox
+    Friend WithEvents ComboBox6 As ComboBox
+    Friend WithEvents ComboBox7 As ComboBox
+    Friend WithEvents ComboBox8 As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ComboBox9 As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ComboBox10 As ComboBox
+    Friend WithEvents ComboBox11 As ComboBox
+    Friend WithEvents ComboBox12 As ComboBox
+End Class
---
+++ WelcomeScreen.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class WelcomeScreen
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Label2 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = SystemColors.Highlight
+        Label1.Font = New Font("Segoe UI", 30F)
+        Label1.Location = New Point(140, 148)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(462, 54)
+        Label1.TabIndex = 0
+        Label1.Text = "Welcome to the TimeBus"
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI", 15F)
+        Button1.Location = New Point(193, 249)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(110, 51)
+        Button1.TabIndex = 1
+        Button1.Text = "Pilot"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Font = New Font("Segoe UI", 15F)
+        Button2.Location = New Point(317, 249)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(110, 51)
+        Button2.TabIndex = 2
+        Button2.Text = "Visitor"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Font = New Font("Segoe UI", 15F)
+        Button3.Location = New Point(441, 249)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(110, 51)
+        Button3.TabIndex = 3
+        Button3.Text = "Guide"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.BackColor = SystemColors.Highlight
+        Label2.Font = New Font("Segoe UI", 13F)
+        Label2.Location = New Point(340, 211)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(73, 25)
+        Label2.TabIndex = 4
+        Label2.Text = "I am a..."
+        ' 
+        ' WelcomeScreen
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = My.Resources.Resources.TimeBus
+        BackgroundImageLayout = ImageLayout.Stretch
+        ClientSize = New Size(743, 438)
+        Controls.Add(Label2)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label1)
+        Name = "WelcomeScreen"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "TimeBus"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label2 As Label
+
+End Class
